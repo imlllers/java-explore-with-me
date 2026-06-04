a02dc0edca8df57bf16854a5c3c5d0fac827ded9
imlllers <vdenskevic@gmail.com>
2026-06-04 03:35:24 +0300

    Этап 2 Версия 6

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -1,7 +1,9 @@
 package ru.practicum.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @RestControllerAdvice
 public class ErrorHandler {
@@ -17,9 +19,18 @@ public class ErrorHandler {
         return new ErrorResponse(e.getMessage());
     }
 
+    @ExceptionHandler({
+            MissingServletRequestParameterException.class,
+            MethodArgumentTypeMismatchException.class
+    })
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleBadRequest(final Exception e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public ErrorResponse handleOther(final Throwable e) {
+    public ErrorResponse handleOther(final Exception e) {
         return new ErrorResponse("Internal server error");
     }
 }
